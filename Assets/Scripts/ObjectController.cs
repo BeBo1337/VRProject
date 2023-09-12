41e48ec5a71f9fa0447099a854fcaee5d420bb23
Dor Edelman <doredelman@gmail.com>
2023-09-12 21:25:36 +0300

    main menu addition
    need to integrate the main menu scene and game better

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class ObjectController : MonoBehaviour
+{
+    // The material to use when this object is inactive (not being gazed at).
+    public Material InactiveMaterial;
+    // The material to use when this object is active (gazed at).
+    public Material GazedAtMaterial;
+
+    private Renderer _myRenderer;
+    [SerializeField] private TMP_Text HiddenText;
+    private const string gameScene = "GameScene";
+    public void Start()
+    {
+        _myRenderer = GetComponent<Renderer>();
+        SetMaterial(false);
+    }
+    public void OnPointerEnter()
+    {
+        SetMaterial(true);
+    }
+    
+    public void OnPointerExit()
+    {
+        SetMaterial(false);
+    }
+    
+    public void OnPointerClick()
+    {
+        // Get the child canvas that has a child TMP text
+        Canvas childCanvas = GetComponentInChildren<Canvas>();
+        if (childCanvas != null)
+        {
+            TMP_Text childTmpText = childCanvas.GetComponentInChildren<TMP_Text>();
+            if (childTmpText != null)
+            {
+                // If the child TMP text has text, then load the game scene
+                if (childTmpText.text == "Play Game")
+                {
+                    SceneManager.LoadScene(gameScene);
+                }
+                else if (childTmpText.text == "Exit")
+                {
+                    // Otherwise, exit the game
+                    Application.Quit();
+                }
+                else
+                {
+                    HiddenText.enabled = true;
+                }
+            }
+        }
+    }
+    
+    private void SetMaterial(bool gazedAt)
+    {
+        if (InactiveMaterial != null && GazedAtMaterial != null)
+        {
+            _myRenderer.material = gazedAt ? GazedAtMaterial : InactiveMaterial;
+        }
+    }
+}
